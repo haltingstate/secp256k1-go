5eafc3d246155af31f2846b4fa9cdb4529119b6b
HaltingState <haltingstate@gmail.com>
2013-11-05 02:36:19 -0800

    unit tests

--- secp256_test.go
+++ secp256_test.go
@@ -2,7 +2,7 @@ package secp256
 
 import (
 	"testing"
-	//"fmt"
+	"fmt"
 )
 
 const TESTS   = 10000 // how many tests
@@ -32,6 +32,7 @@ func Test_Secp256_02s(t *testing.T) {
 	if len(pubkey) != 33 {t.Fail()}
 	if len(seckey) != 32 {t.Fail()}
 	if len(sig) != 64+1 {t.Fail()}
+	if int(sig[64]) > 4 {t.Fail()} //should be 0 to 4
 }
 
 //test signing message
@@ -41,6 +42,23 @@ func Test_Secp256_02(t *testing.T) {
 	sig := Sign(msg, seckey)
 	ret := VerifySignature(msg, sig)
 	if ret != 1 {  t.Fatal("Signature invalid") }
+}
+
+//test pubkey recovery
+func Test_Secp256_02a(t *testing.T) { 
+	pubkey1,seckey1 := GenerateKeyPair()
+	msg := RandByte(32)
+	sig := Sign(msg, seckey1)
+	ret := VerifySignature(msg, sig)
+	if ret != 1 {  t.Fatal("Signature invalid") }
+
+	pubkey2 := RecoverPubkey(msg, sig)
+	if len(pubkey1) != len(pubkey2) {t.Fatal()}
+	for i,_ := range pubkey1 { 
+		if pubkey1[i] != pubkey2[i] {
+			t.Fatal()
+		} 
+	}
 }
 
 
@@ -66,12 +84,13 @@ func Test_Secp256_04(t *testing.T) {
 	}
 }
 
-//test random signatures that should fail
+//test random signatures against fixed messages; should fail
 
 //crashes: 
 //	-SIPA look at this
-/*
-func Test_Secp256_05(t *testing.T) { 
+
+
+func Test_Secp256_06a(t *testing.T) { 
 	_,seckey := GenerateKeyPair()
 	msg := RandByte(32)
 	sig := Sign(msg, seckey)
@@ -79,19 +98,59 @@ func Test_Secp256_05(t *testing.T) {
 		sig = RandByte(len(sig))
 		//sig[len(sig)-1] %= 4
 		ret := VerifySignature(msg, sig)
-		if ret == 1 { t.Fail()}
+		if ret == 1 { t.FailNow()}
 	}
 }
+
+
+//test random messages against valid signature: should fail
+//crashes
+// -SIPA look at this
+
+/*
+--- FAIL: Test_Secp256_06b (2.08 seconds)
+secp256.test: /home/atomos/secp256/./secp256k1/src/impl/num_gmp.h:55: secp256k1_num_get_bin: Assertion `len-shift <= rlen' failed.
+SIGABRT: abort
+PC=0x7fe6237def77
+signal arrived during cgo execution
 */
 
-//test random messages that should fail
-func Test_Secp256_06(t *testing.T) { 
+
+func Test_Secp256_06b(t *testing.T) { 
 	_,seckey := GenerateKeyPair()
 	msg := RandByte(32)
 	sig := Sign(msg, seckey)
+
+	fail_count := 0
 	for i:=0; i<TESTS; i++ {
 		msg = RandByte(32)
 		ret := VerifySignature(msg, sig)
-		if ret == 1 { t.Fail()}
+		if ret == 1 { 
+			fail_count++;
+			t.FailNow()
+		}
+	}
+	if fail_count != 0 {
+		fmt.Printf("ERROR: Accepted signature for %v of %v random messages\n", fail_count, TESTS)
+	}
+}
+
+
+//test random messages against random signatures: should fail
+
+func Test_Secp256_06c(t *testing.T) { 
+	fail_count := 0
+	for i:=0; i<TESTS; i++ {
+		sig := RandByte(65)
+		sig[64] %= 4; 
+		msg := RandByte(32)
+		ret := VerifySignature(msg, sig)
+		if ret == 1 { 
+			fail_count++;
+			t.FailNow()
+		}
+	}
+	if fail_count != 0 {
+		fmt.Printf("ERROR: Accepted signature for %v of %v random messages\n", fail_count, TESTS)
 	}
 }
