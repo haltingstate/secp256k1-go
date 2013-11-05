efa6596e69c5bb9e4edf8b310c96ef3eb27cd1e6
HaltingState <haltingstate@gmail.com>
2013-11-05 01:05:26 -0800

    stuff

--- secp256_rand.go
+++ secp256_rand.go
@@ -1,20 +1,18 @@
 package secp256
 
 import (
-    //"math/rand"
+    "io"
+    "crypto/rand"
 )
-
-import "crypto/rand"
-
 
 
 //completely insecure random number generator
 //use entropy pool etc and cryptographic random number generator
 func RandByte(n int) []byte {
-    b := make([]byte, n)
-    buff, err := io.ReadFull(rand.Reader, n)
-    if len(buff) != n || err != nil {
+    buff := make([]byte, n)
+    ret, err := io.ReadFull(rand.Reader, buff)
+    if len(buff) != ret || err != nil {
         return nil
     }
-
+    return buff
 }
