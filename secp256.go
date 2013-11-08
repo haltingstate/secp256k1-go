4ea01d71c956fbe34ca595bf998d4bb5c1f4493f
HaltingState <haltingstate@gmail.com>
2013-11-08 11:41:07 -0800

    sipa library passes unit tests

--- secp256.go
+++ secp256.go
@@ -1,4 +1,5 @@
 package secp256
+
 /*
 #cgo CFLAGS: -std=gnu99 -Wno-error
 #cgo LDFLAGS: -lgmp
@@ -10,11 +11,13 @@ package secp256
 import "C"
 
 import (
-        "unsafe"
-//    "fmt"
-        //"errors"
-        "log"
+	"unsafe"
+	//    "fmt"
+	//"errors"
+	"bytes"
+	"log"
 )
+
 //#include "./src/secp256k1.c"
 //removing the "-std=std99" or replacing it with "-std=gnu99"
 //#cgo LDFLAGS: -L. -L./lib -L../../../lib -Wl,-rpath='./lib/' -lsecp256k1 -lgmp
@@ -32,38 +35,37 @@ import (
 */
 
 /*
-    Todo:
-    > Centralize key management in module
-    > add pubkey/private key struct
-    > Dont let keys leave module; address keys as ints
+   Todo:
+   > Centralize key management in module
+   > add pubkey/private key struct
+   > Dont let keys leave module; address keys as ints
 
-    > store private keys in buffer and shuffle (deters persistance on swap disc)
-    > Byte permutation (changing)
-    > xor with chaning random block (to deter scanning memory for 0x63) (stream cipher?)
-    > randomize buffer size to between 16 MB and 32MB and multiple of 4096 bytes
+   > store private keys in buffer and shuffle (deters persistance on swap disc)
+   > Byte permutation (changing)
+   > xor with chaning random block (to deter scanning memory for 0x63) (stream cipher?)
+   > randomize buffer size to between 16 MB and 32MB and multiple of 4096 bytes
 
-    On Disk
-    > Store keys in wallets
-    > use slow key derivation function for wallet encryption key (2 seconds)
+   On Disk
+   > Store keys in wallets
+   > use slow key derivation function for wallet encryption key (2 seconds)
 */
 
 /*
  For instance, nonces are used in HTTP digest access authentication to calculate an MD5 digest
- of the password. The nonces are different each time the 401 authentication challenge 
+ of the password. The nonces are different each time the 401 authentication challenge
  response code is presented, thus making replay attacks virtually impossible.
 
 can verify client/server match without sending password over network
 */
 
-
 //void secp256k1_start(void);
 func init() {
-    C.secp256k1_start()     //takes 10ms to 100ms
+	C.secp256k1_start() //takes 10ms to 100ms
 }
 
 // void secp256k1_stop(void);
 func Stop() {
-    C.secp256k1_stop()
+	C.secp256k1_stop()
 }
 
 /*
@@ -97,7 +99,7 @@ func Stop() {
 
 /*
 int secp256k1_ecdsa_pubkey_create(
-    unsigned char *pubkey, int *pubkeylen, 
+    unsigned char *pubkey, int *pubkeylen,
     const unsigned char *seckey, int compressed);
 */
 
@@ -116,27 +118,26 @@ int secp256k1_ecdsa_pubkey_create(
 
 func GenerateKeyPair() ([]byte, []byte) {
 
-    pubkey_len := C.int(33)
-    const seckey_len = 32
+	pubkey_len := C.int(33)
+	const seckey_len = 32
 
-    var pubkey []byte = make([]byte, pubkey_len);
-    var seckey []byte = RandByte(seckey_len) //going to get bitcoins stolen!
+	var pubkey []byte = make([]byte, pubkey_len)
+	var seckey []byte = RandByte(seckey_len) //going to get bitcoins stolen!
 
-    var pubkey_ptr *C.uchar = (*C.uchar)(unsafe.Pointer(&pubkey[0]))
-    var seckey_ptr *C.uchar = (*C.uchar)(unsafe.Pointer(&seckey[0]))
+	var pubkey_ptr *C.uchar = (*C.uchar)(unsafe.Pointer(&pubkey[0]))
+	var seckey_ptr *C.uchar = (*C.uchar)(unsafe.Pointer(&seckey[0]))
 
-    ret := C.secp256k1_ecdsa_pubkey_create(
-        pubkey_ptr, &pubkey_len,
-        seckey_ptr,1)
+	ret := C.secp256k1_ecdsa_pubkey_create(
+		pubkey_ptr, &pubkey_len,
+		seckey_ptr, 1)
 
-    if ret != 1 {
-        return GenerateKeyPair() //invalid secret, try again
-    }
-    return pubkey, seckey
+	if ret != 1 {
+		return GenerateKeyPair() //invalid secret, try again
+	}
+	return pubkey, seckey
 }
 
-
-/* 
+/*
 *  Create a compact ECDSA signature (64 byte + recovery id).
 *  Returns: 1: signature created
 *           0: nonce invalid, try another one
@@ -146,7 +147,7 @@ func GenerateKeyPair() ([]byte, []byte) {
 *           nonce:  pointer to a 32-byte nonce (generated with a cryptographic PRNG)
 *  Out:     sig:    pointer to a 64-byte array where the signature will be placed.
 *           recid:  pointer to an int, which will be updated to contain the recovery id.
-*/
+ */
 
 /*
 int secp256k1_ecdsa_sign_compact(const unsigned char *msg, int msglen,
@@ -157,34 +158,34 @@ int secp256k1_ecdsa_sign_compact(const unsigned char *msg, int msglen,
 */
 
 func Sign(msg []byte, seckey []byte) []byte {
-    var nonce []byte = RandByte(32) //going to get bitcoins stolen!
+	var nonce []byte = RandByte(32) //going to get bitcoins stolen!
 
-    var sig []byte = make([]byte,65)
-    var recid C.int;
+	var sig []byte = make([]byte, 65)
+	var recid C.int
 
-    var msg_ptr *C.uchar = (*C.uchar)(unsafe.Pointer(&msg[0]))
-    var seckey_ptr *C.uchar = (*C.uchar)(unsafe.Pointer(&seckey[0]))
-    var nonce_ptr *C.uchar = (*C.uchar)(unsafe.Pointer(&nonce[0]))
-    var sig_ptr *C.uchar = (*C.uchar)(unsafe.Pointer(&sig[0]))
+	var msg_ptr *C.uchar = (*C.uchar)(unsafe.Pointer(&msg[0]))
+	var seckey_ptr *C.uchar = (*C.uchar)(unsafe.Pointer(&seckey[0]))
+	var nonce_ptr *C.uchar = (*C.uchar)(unsafe.Pointer(&nonce[0]))
+	var sig_ptr *C.uchar = (*C.uchar)(unsafe.Pointer(&sig[0]))
 
-    if C.secp256k1_ecdsa_seckey_verify(seckey_ptr) != C.int(1) {
-        log.Panic() //invalid seckey
-    }
+	if C.secp256k1_ecdsa_seckey_verify(seckey_ptr) != C.int(1) {
+		log.Panic() //invalid seckey
+	}
 
-    ret := C.secp256k1_ecdsa_sign_compact(
-        msg_ptr, C.int(len(msg)),
-        sig_ptr,
-        seckey_ptr,
-        nonce_ptr,
-        &recid);
+	ret := C.secp256k1_ecdsa_sign_compact(
+		msg_ptr, C.int(len(msg)),
+		sig_ptr,
+		seckey_ptr,
+		nonce_ptr,
+		&recid)
 
-    sig[64] = byte(int(recid))
+	sig[64] = byte(int(recid))
 
-    if ret != 1 {
-        return Sign(msg,seckey) //nonce invalid,retry
-    }
+	if ret != 1 {
+		return Sign(msg, seckey) //nonce invalid,retry
+	}
 
-    return sig
+	return sig
 
 }
 
@@ -193,36 +194,58 @@ func Sign(msg []byte, seckey []byte) []byte {
 *  Returns: 1: secret key is valid
 *           0: secret key is invalid
 *  In:      seckey: pointer to a 32-byte secret key
-*/
+ */
 func VerifySeckey(seckey []byte) int {
-    if len(seckey) != 32 {return 0}
-    var seckey_ptr *C.uchar = (*C.uchar)(unsafe.Pointer(&seckey[0]))
-    ret := C.secp256k1_ecdsa_seckey_verify(seckey_ptr)
-    return int(ret)
+	if len(seckey) != 32 {
+		return 0
+	}
+	var seckey_ptr *C.uchar = (*C.uchar)(unsafe.Pointer(&seckey[0]))
+	ret := C.secp256k1_ecdsa_seckey_verify(seckey_ptr)
+	return int(ret)
 }
 
 /*
 * Validate a public key.
 *  Returns: 1: valid public key
 *           0: invalid public key
-*/
+ */
 
 func VerifyPubkey(pubkey []byte) int {
-    if len(pubkey) != 33 {return 0}
-    var pubkey_ptr *C.uchar = (*C.uchar)(unsafe.Pointer(&pubkey[0]))
-    ret := C.secp256k1_ecdsa_pubkey_verify(pubkey_ptr, 33)
-    return int(ret)
+	if len(pubkey) != 33 {
+		return 0
+	}
+	var pubkey_ptr *C.uchar = (*C.uchar)(unsafe.Pointer(&pubkey[0]))
+	ret := C.secp256k1_ecdsa_pubkey_verify(pubkey_ptr, 33)
+	return int(ret)
 }
-
 
 //for compressed signatures, does not need pubkey
 func VerifySignature(msg []byte, sig []byte, pubkey1 []byte) int {
-    pubkey2 := RecoverPubkey(msg, sig) //if pubkey recovered, signature valid
+	if msg == nil || sig == nil || pubkey1 == nil {
+		log.Panic("ERROR: invalid input, nils")
+	}
+	if len(sig) != 65 {
+		log.Panic("invalid signature length")
+	}
+	if len(pubkey1) != 33 {
+		log.Panic("invalid pubkey length")
+	}
 
-    if len(pubkey1) != 33 || len(pubkey2) != 33 { return 0;}
-    for i:=0; i<33; i++ { if(pubkey1[i] != pubkey2[i]) {return 0;}}
-    if pubkey2 != nil { return 1 }
-    return 0
+	pubkey2 := RecoverPubkey(msg, sig) //if pubkey recovered, signature valid
+
+	if pubkey2 == nil {
+		return 0
+	}
+
+	if len(pubkey2) != 33 {
+		log.Panic("recovered pubkey length invalid")
+	}
+
+	if bytes.Equal(pubkey1, pubkey2) == true {
+		return 1 //valid signature
+	}
+
+	return 0
 }
 
 /*
@@ -246,39 +269,40 @@ int secp256k1_ecdsa_recover_compact(const unsigned char *msg, int msglen,
 
 //recovers the public key from the signature
 //recovery of pubkey means correct signature
-func RecoverPubkey(msg []byte, sig []byte) ([]byte) {
-    if len(sig) != 65 {log.Panic()}
+func RecoverPubkey(msg []byte, sig []byte) []byte {
+	if len(sig) != 65 {
+		log.Panic()
+	}
 
-    var pubkey []byte = make([]byte, 33)
+	var pubkey []byte = make([]byte, 33)
 
-    var msg_ptr *C.uchar = (*C.uchar)(unsafe.Pointer(&msg[0]))
-    var sig_ptr *C.uchar = (*C.uchar)(unsafe.Pointer(&sig[0]))
-    var pubkey_ptr *C.uchar = (*C.uchar)(unsafe.Pointer(&pubkey[0]))
+	var msg_ptr *C.uchar = (*C.uchar)(unsafe.Pointer(&msg[0]))
+	var sig_ptr *C.uchar = (*C.uchar)(unsafe.Pointer(&sig[0]))
+	var pubkey_ptr *C.uchar = (*C.uchar)(unsafe.Pointer(&pubkey[0]))
 
-    var pubkeylen C.int;
+	var pubkeylen C.int
 
-    ret := C.secp256k1_ecdsa_recover_compact(
-        msg_ptr, C.int(len(msg)),
-        sig_ptr,
-        pubkey_ptr, &pubkeylen,
-        C.int(1), C.int(sig[64]),
-    );
+	ret := C.secp256k1_ecdsa_recover_compact(
+		msg_ptr, C.int(len(msg)),
+		sig_ptr,
+		pubkey_ptr, &pubkeylen,
+		C.int(1), C.int(sig[64]),
+	)
 
-    if ret == 0 || int(pubkeylen) != 33 {
-        return nil
-    }
+	if ret == 0 || int(pubkeylen) != 33 {
+		return nil
+	}
 
-    return pubkey
+	return pubkey
 
 }
-
 
 /* Verify an ECDSA signature.
 *  Returns: 1: correct signature
 *           0: incorrect signature
 *          -1: invalid public key
 *          -2: invalid signature
-*/
+ */
 /*
 for non-compressed
 func VerifySignature(msg []byte, sig []byte, pubkey []byte ) int {
